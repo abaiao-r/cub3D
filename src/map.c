a5c2be556509486af3854b95b5501d7ed87b4b0f
Pedro Manuel Pereira Coelho Bigote <pedperei@c1r13s3.42lisboa.com>
2023-07-07 20:12:24 +0100

    1

--- src/map.c
+++ src/map.c
@@ -6,7 +6,7 @@
 /*   By: pedperei <pedperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 16:30:51 by pedperei          #+#    #+#             */
-/*   Updated: 2023/07/07 17:14:28 by pedperei         ###   ########.fr       */
+/*   Updated: 2023/07/07 20:09:43 by pedperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,37 @@ int	count_lines(char *map)
 	close(fd);
 	return (i);
 }
+
+int	ft_strlen_mod(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+int	count_cols(char *map)
+{
+	int		fd;
+	char	*str;
+	int		len;
+
+	fd = open(map, O_RDONLY);
+	str = get_next_line(fd);
+	len = 0;
+	while (str != NULL)
+	{
+		if (ft_strlen_mod(str) > len)
+			len = ft_strlen_mod(str);
+		free(str);
+		str = get_next_line(fd);
+	}
+	close(fd);
+	return (len);
+}
+
 
 /*Aloca memória para gravar o mapa como array de strings (char **)
 utiliza-se a funcao get_next_line para ir lendo as linhas 
@@ -74,7 +105,7 @@ int	check_chars(t_map *map)
 		while (map->game_map[i][j] != '\n' && map->game_map[i][j] != '\0')
 		{
 			if (map->game_map[i][j] == 'N' || map->game_map[i][j] == 'S'
-                || map->game_map[i][j] == 'W' || map->game_map[i][j] == 'E')
+				|| map->game_map[i][j] == 'W' || map->game_map[i][j] == 'E')
 			{
 				map->count_player++;
 				map->player_c = j;
@@ -88,27 +119,42 @@ int	check_chars(t_map *map)
 	return (1);
 }
 
+int	is_player_empty(char c)
+{
+	if (c == 'N' || c == 'E' || c == 'S' || c == 'W' || c == '0')
+		return (1);
+	return (0);
+}
+
+int is_surrounded(t_map *map, int pos_l, int pos_c)
+{
+	if (!(pos_l >= 0 && pos_l < map->lin))
+		return (0);
+	map->col = ft_strlen_mod(map->game_map[pos_l - 1]);
+	if (!(pos_c >= 0 && pos_c < map->col))
+		return (0);
+	if (map->game_map[pos_l][pos_c] == '1')
+		return (1);
+	is_surrounded(map, pos_l + 1, pos_c);
+	is_surrounded(map, pos_l, pos_c + 1);
+	is_surrounded(map, pos_l - 1, pos_c);
+	is_surrounded(map, pos_l, pos_c - 1);
+	return (0);
+}
+
 int	is_wall(t_map *map)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (i < map->lin)
+	while (map->game_map[i])
 	{
 		j = 0;
 		while (map->game_map[i][j] != '\n' && map->game_map[i][j] != '\0')
 		{
-			if (i == 0 || i == (map->lin - 1))
-			{
-				if (map->game_map[i][j] != '1')
-					return (0);
-			}
-			if (j == 0 || j == (map->col - 1))
-			{
-				if (map->game_map[i][j] != '1')
-					return (0);
-			}
+			if (is_player_empty(map->game_map[i][j]))
+				is_surrounded(map, i, j);
 			j++;
 		}
 		i++;
@@ -128,16 +174,22 @@ t_map	*init_map(char **str_map)
 	if (!map->game_map)
 		return (NULL);
 	map->lin = count_lines(str_map[1]);
+	map->col = count_cols(str_map[1]);
 	//if (!is_rectangule(map))
 	//	return (free_t_map(map, 'r'));
 	if (!is_wall(map))
+	{
+		printf("MAP not OK\n");
 		return (NULL);
+	}
+	else
+		printf("MAP OK\n");
 	return (map);
 }
 
 int	main(int argc, char **argv)
 {
-	t_cub	*cub;
+	t_cub *cub;
 
 	(void)argc;
 	cub = (t_cub *)ft_calloc(1, sizeof(t_cub));
@@ -149,11 +201,10 @@ int	main(int argc, char **argv)
 		free(cub);
 		return (0);
 	}
-    int i = 0;
-    while (cub->map->game_map[i])
-    {
-        printf("%s", cub->map->game_map[i]);
-        i++;
-    }
-    
+	int i = 0;
+	while (cub->map->game_map[i])
+	{
+		printf("%s", cub->map->game_map[i]);
+		i++;
+	}
 }
