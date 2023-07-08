37d1a8e74bb403f27d363176bc17e2d0a0cf0001
Pedro Manuel Pereira Coelho Bigote <pedperei@c2r8s2.42lisboa.com>
2023-07-08 18:41:30 +0100

    check elements merged | (desc_file vs map_file)

--- src/map_elements_check.c
+++ src/map_elements_check.c
@@ -3,65 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   map_elements_check.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedperei <pedperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:28:55 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/07/08 18:18:15 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/07/08 18:41:04 by pedperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../includes/cub3d.h"
 
-int check_elements(t_map *map)
+int	check_elements(t_map *map)
 {
+	char *elements[6];
+	int i;
+	int j;
+	int num_elements;
+	int count_elements;
+	char *element;
+	int element_len;
 
-    char *elements[6];
-    int i;
-    int j;
-    int num_elements;
-    int count_elements;
-    char *element;
-    int element_len;
+	elements[0] = "NO";
+	elements[1] = "SO";
+	elements[2] = "WE";
+	elements[3] = "EA";
+	elements[4] = "F";
+	elements[5] = "C";
+	num_elements = sizeof(elements) / sizeof(elements[0]);
 
-    elements[0] = "NO";
-    elements[1] = "SO";
-    elements[2] = "WE";
-    elements[3] = "EA";
-    elements[4] = "F";
-    elements[5] = "C";
-    num_elements = sizeof(elements) / sizeof(elements[0]);
-
-    i = 0;
-    count_elements = 0;
-    while (i < num_elements)
-    {
-        element = elements[i];
-        element_len = ft_strlen(element);
-        j = 0;
-        while (j < map->lin)
-        {
-            ft_skip_whitespace(&map->desc_file[j]);
-            if (ft_strncmp(map->desc_file[j], element, element_len) == 0)
-            {
-                count_elements++;
-                if (count_elements == num_elements)
-                    map->elements_end = j;
-            }
-            else if(count_elements < num_elements && ft_strncmp(map->desc_file[j], element, element_len) != 0)
-            {
-                printf("Error: Invalid element\n");
-                return 0;
-            }
-            j++;
-        }
-        i++;
-    }
-    if (count_elements != num_elements)
-    {
-        printf("Error: Missing element(s)\n");
-        return 0;
-    }
-    printf("OK\n");
-    return (1);
+	i = 0;
+	count_elements = 0;
+	while (i < num_elements)
+	{
+		element = elements[i];
+		element_len = ft_strlen(element);
+		j = 0;
+		while (j < map->lin)
+		{
+			ft_skip_whitespace(&map->desc_file[j]);
+			if (ft_strncmp(map->desc_file[j], element, element_len) == 0)
+			{
+				count_elements++;
+				if (count_elements == num_elements)
+					map->elements_end = j;;
+			}
+			else if (count_elements < num_elements && ft_strncmp(map->desc_file[j], element, element_len) != 0)
+			{
+				printf("Error: Invalid element\n");
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	if (count_elements != num_elements)
+	{
+		printf("Error: Missing element(s)\n");
+		return (0);
+	}
+	printf("OK\n");
+	return (1);
 }
